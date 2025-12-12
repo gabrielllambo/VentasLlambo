70d75a8191b0ef13a36d1a01bc47829f1a2b1e25
Gabriel Llambo <gabrielllambo998@gmail.com>
2025-12-12 15:36:38 -0500

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  //api_url: 'https://api-ventas-aaavhsg2bjg8hndm.eastus2-01.azurewebsites.net'
-  api_url: 'http://localhost:5285'
+  api_url: 'https://api-ventas-aaavhsg2bjg8hndm.eastus2-01.azurewebsites.net'
+  //api_url: 'http://localhost:5285'
 };
